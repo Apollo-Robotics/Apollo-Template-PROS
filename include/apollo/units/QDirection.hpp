9e90cf79507c211b81601999faf9bc972a32dbfa
Nikhil Gajjar <nikgajjar51@gmail.com>
2023-05-25 15:41:42 -0400

    Adding cardinal directions

--- include/apollo/units/QDirection.hpp
+++ include/apollo/units/QDirection.hpp
@@ -7,4 +7,12 @@
 
 #include "apollo/units/RQuantity.hpp"
 namespace apollo {
+enum direction { FORWARD = 0,
+                 FWD = FORWARD,
+                 REVERSE = 1,
+                 REV = REVERSE,
+                 LEFT = 2,
+                 L = LEFT,
+                 RIGHT = 3,
+                 R = RIGHT };
 }
